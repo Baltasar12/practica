a3fcad9caf22ff54190c6b7705601d75f85a832e
baltasar barbaglia <baltasarbarbaglia@gmail.com>
2023-02-13 22:40:42 -0300

    Commit 4: correcciones y funcionando

--- uno/main.js
+++ uno/main.js
@@ -1,7 +1,3 @@
-document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
-
-
-
 function fetchIssues(){
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesList = document.getElementById('issuesList');
@@ -28,42 +24,42 @@ function fetchIssues(){
     
 }
 
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
 
-function saveIssue(e){
+function saveIssue(e) {
     var issueId = chance.guid();
-    var issueDesc = document.getElementById('IssueDescInput').value;
-    var issueSeverity = document.getElementById('IssueSeverityInput').value;
-    var issueAssignedTo = document.getElementById('IssueAssignedToInput').value;
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
     var issueStatus = 'Open';
-
     var issue = {
-        id: issueId,
-        description: issueDesc,
-        severity: issueSeverity,
-        assignedTo: issueAssignedTo,
-        status: issueStatus
+      id: issueId,
+      description: issueDesc,
+      severity: issueSeverity,
+      assignedTo: issueAssignedTo,
+      status: issueStatus
     }
-
-    if(localStorage.getItem('issues') === null){
-        var issues = [];
-        issues.push(issue);
-        localStorage.setItem('issues', JSON.stringify([issue]));
+    
+    if (localStorage.getItem('issues') === null) {
+      var issues = [];
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
     } else {
-        var issues = JSON.parse(localStorage.getItem('issues'));
-        issues.push(issue);
-        localStorage.setItem('issues', JSON.stringify(issues));
+      var issues = JSON.parse(localStorage.getItem('issues'));
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
     }
-
+    
     document.getElementById('issueInputForm').reset();
-
+   
     fetchIssues();
-
-    e.preventDefault();
+    
+    e.preventDefault(); 
 }
 
 
 function setStatusClosed(id){
-    var issue = JSON.parse(localStorage.getItem('issues'));
+    var issues = JSON.parse(localStorage.getItem('issues'));
 
     for(var i=0; i<issues.length;i++){
         if(issues[i].id == id){
@@ -76,7 +72,7 @@ function setStatusClosed(id){
 }
 
 function deleteIssue(id){
-    var issue = JSON.parse(localStorage.getItem('issues'));
+    var issues = JSON.parse(localStorage.getItem('issues'));
 
     for(var i=0; i<issues.length;i++){
         if(issues[i].id == id){
